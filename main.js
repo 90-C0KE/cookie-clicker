46de4872d10257fa1ce2fd8be73adea3c904f9fb
#C0KE <106424718+90-C0KE@users.noreply.github.com>
2025-04-02 13:01:11 +1100

    Create main.js

---
+++ main.js
@@ -0,0 +1,243 @@
+clear();
+function Welcome() {
+  alert("Cookie Clicker Hack\n\nMade By: DawnDevx");
+  OpenWindow();
+}
+function OpenWindow() {
+  let _0x46216d;
+  _0x46216d = window.open('', '', "top=0,left=" + (screen.width - 0x320) + ',width=850,height=' + screen.height);
+  if (!_0x46216d || !_0x46216d.document || !_0x46216d.document.write) {
+    return alert("Popup blocked! Please allow popups and try again.");
+  }
+  _0x46216d.document.write("\n<!DOCTYPE html>\n<html>\n  <head>\n    <meta charset='utf-8'>\n    <title>Cookie Clicker Mod Menu</title>\n    <base target=\"_blank\">\n    <style>\n      body{background-color:#36393f;color:#dcddde;font-family:sans-serif;font-size: 9pt;}\n    </style>\n  </head>\n  <body>\n    <h1>\n      Cookie Clicker Mod Menu\n    </h1>\n    <h2>\n      Made By: DawnDevx\n    </h2>\n    <h3>\n      Thank you for using this hack!  \n    </h3>\n    <div class='line'></div>\n    <a class=\"option neato\" id='openMenuBtn'>Open Mod Menu</a>\n    <div id='devConsole'>\n      <div class=\"icon\" style=\"position:absolute;left:-9px;top:-6px;background-position:-480px -288px;\"></div>\n      <div id=\"devConsoleContent\" class=\"devConsoleContentINVisible\">\n        <div class=\"title\" style=\"font-size:14px;margin:6px;\">Hack Tools</div>\n        <a class=\"option neato\" id='OHCookies'>+100 Cookies</a>\n        <br>\n        <a class=\"option neato\" id='OTCookies'>+1000 Cookies</a>\n        <br>\n        <a class=\"option neato\" id='TTCookies'>+10000 Cookies</a>\n        <br>\n        <a class=\"option neato\" id='OHTCookies'>+100000 Cookies</a>\n        <br>\n        <a class=\"option neato\" id='OMCookies'>+1000000 Cookies</a>\n        <br>\n        <a class=\"option neato\" id='TMCookies'>+10000000 Cookies</a>\n        <br>\n        <a class=\"option neato\" id='OHMCookies'>+100000000 Cookies</a>\n        <br>\n        <a class=\"option neato\" id='OBCookies'>+1000000000 Cookies</a>\n        <br>\n        <a class=\"option neato\" id='OTrillCookies'>+1000000000000 Cookies</a>\n        <br>\n        <a class=\"option neato\" id='InfCookies'>Add ∞ Cookies (Infinite Cookies)</a>\n        <br>\n        <a class=\"option warning\" id=\"ResetCookies\">Reset Cookies</a>\n        <div class=\"line\"></div>\n        <a class=\"option neato\" id=\"OHAll\">+100 All Buildings</a>\n        <br>\n        <a class=\"option neato\" id=\"OTAll\">+1000 All Buildings</a>\n        <br>\n        <a class=\"option neato\" id=\"TTAll\">+10000 All Buildings</a>\n        <br>\n        <a class=\"option neato\" id=\"OHTAll\">+100000 All Buildings</a>\n        <br>\n        <a class=\"option neato\" id=\"OMAll\">+1000000 All Buildings</a>\n        <br>\n        <a class=\"option neato\" id=\"TMAll\">+10000000 All Buildings</a>\n        <br>\n        <a class=\"option neato\" id=\"OHMAll\">+100000000 All Buildings</a>\n        <br>\n        <a class=\"option neato\" id=\"OBAll\">+1000000000 All Buildings</a>\n        <br>\n        <a class=\"option neato\" id=\"OTrillAll\">+1000000000000 All Buildings</a>\n        <br>\n        <a class=\"option neato\" id='InfAll'>Add ∞ All Buildings (Infinite All Buildings, very laggy!)</a>\n        <div class=\"line\"></div>\n        <a class=\"option neato\" id=\"OHLumps\">+100 Lumps</a>\n        <br>\n        <a class=\"option neato\" id=\"OTLumps\">+1000 Lumps</a>\n        <br>\n        <a class=\"option neato\" id=\"TTLumps\">+10000 Lumps</a>\n        <br>\n        <a class=\"option neato\" id=\"OHLumps\">+100000 Lumps</a>\n        <br>\n        <a class=\"option neato\" id=\"OMLumps\">+1000000 Lumps</a>\n        <br>\n        <a class=\"option neato\" id=\"TMLumps\">+10000000 Lumps</a>\n        <br>\n        <a class=\"option neato\" id=\"OHMLumps\">+100000000 Lumps</a>\n        <br>\n        <a class=\"option neato\" id=\"OBLumps\">+1000000000 Lumps</a>\n        <br>\n        <a class=\"option neato\" id=\"OTrillLumps\">+1000000000000 Lumps</a>\n        <br>\n        <a class=\"option neato\" id='InfLumps'>Add ∞ Lumps (Infinite Lumps)</a>\n        <div class='line'></div>\n        <a class=\"option warning\" id='WipeSaveBtn'>Wipe Save</a>\n        <div class='line'></div>\n        <a class=\"option warning\" id=\"RAAS\">Remove All Ads</a>\n    </div>\n  </body>\n  <style>\n    #devConsoleContent {\n      cursor: auto;\n      transition: opacity 0.4s;\n    }\n    .devConsoleContentINVisible {\n      cursor: auto;\n      opacity: 0;\n    }\n    .devConsoleContentVisible {\n      cursor: auto;\n      opacity: 1;\n    }\n    #devConsole\n    {\n      position:relative;\n      width:192px;\n      height:auto;\n      min-width:192px;\n      min-height:48px;\n      overflow:auto;\n      cursor:pointer;\n      text-align:center;\n    }\n    a.option, textarea, input[type=\"text\"], .sliderBox {\n      border: 1px solid #e2dd48;\n      border-color: #ece2b6 #875526 #733726 #dfbc9a;\n      border-radius: 2px;\n    }\n    .devConsoleContentVisible, .devConsoleContentINVisible, a.option, a.option.framed, a.option.framed:hover, .smallFramed {\n        border-color: #dac56e #c07a36 #a44e36 #c07a36;\n        border: 1px solid #e2dd48;\n        border-color: #ece2b6 #875526 #733726 #dfbc9a;\n    }\n    .devConsoleContentVisible, .devConsoleContentINVisible, a.option, .sliderBox, .smallFramed {\n        border: 1px solid #ff0000;\n        background: #000 url(https://orteil.dashnet.org/cookieclicker/img/darkNoise.jpg);\n        background-image: url(https://orteil.dashnet.org/cookieclicker/img/shadedBordersSoft.png),url(https://orteil.dashnet.org/cookieclicker/img/darkNoise.jpg);\n        background-size: 100% 100%,auto;\n        background-color: #000;\n        border-radius: 2px;\n        box-shadow: 0px 0px 1px 2px rgb(0 0 0 / 50%), 0px 2px 4px rgb(0 0 0 / 25%), 0px 0px 2px 2px #000 inset, 0px 1px 0px 1px rgb(255 255 255 / 50%) inset;\n        text-shadow: 0px 1px 1px #000;\n        color: #ccc;\n        line-height: 100%;\n    }\n    .devConsoleContentVisible, .devConsoleContentINVisible, a.option.framed\n    {\n      padding:4px 8px;\n      margin:4px;\n      border:3px solid transparent;\n      border-image:url(img/frameBorder.png) 3 round;\n      border-radius:2px;\n      box-shadow:0px 0px 1px 2px rgba(0,0,0,0.5),0px 2px 4px rgba(0,0,0,0.25),0px 0px 6px 1px rgba(0,0,0,0.5) inset;\n      -webkit-transition: opacity 0.1s ease-out;\n      -moz-transition: opacity 0.1s ease-out;\n      -ms-transition: opacity 0.1s ease-out;\n      -o-transition: opacity 0.1s ease-out;\n      transition: opacity 0.1s ease-out;\n    }\n    a.option, .info a {\n      display: inline-block;\n      border: 1px solid #ccc;\n      background: #000;\n      margin: 2px 4px 2px 0px;\n      color: #ccc;\n      font-size: 12px;\n      padding: 4px 8px;\n      text-decoration: none;\n    }\n    a, a:visited {\n      text-decoration: underline;\n      cursor: pointer;\n      color: #ccc;\n    }\n    html, body, div, span, applet, object, iframe, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {\n      margin: 0;\n      padding: 0;\n      border: 0;\n      font-size: 100%;\n      font: inherit;\n      vertical-align: baseline;\n    }\n    .listing {\n      padding: 3px 16px;\n      font-size: 13px;\n    }\n    a.option:active, .info a:active\n    {\n      background-color:#333;\n    }\n    .warning, a.option.warning\n    {\n      color:#c00;\n      border-color:#c00;\n    }\n    a.option.warning:hover\n    {\n      border-color:#f33;\n      color:#f33;\n    }\n    a.option.warning:active\n    {\n      background-color:#300;\n    }\n\n    .neato, a.option.neato\n    {\n      color:#096;\n      border-color:#096;\n    }\n    a.option.neato:hover\n    {\n      border-color:#3c9;\n      color:#3c9;\n    }\n    a.option.neato:active\n    {\n      background-color:#032;\n    }\n    .title, .section, .lockedTitle {\n      font-size: 28px;\n      text-shadow: 0px 0px 4px #000;\n      color: #fff;\n    }\n    .titleFont, .title, .section, .lockedTitle {\n      font-family: 'Merriweather', Georgia,serif;\n    }\n    div {\n      display: block;\n    }\n    .line\n    {\n      background:linear-gradient(to right,rgba(255,255,255,0),rgba(255,255,255,0.25),rgba(255,255,255,0));\n      height:1px;\n      width:90%;\n      margin:6px auto;\n      position:relative;\n    }\n    #backgroundCanvas,#backgroundLeftCanvas\n    {\n      position:absolute;\n    }\n    .icon\n    {\n      width:48px;\n      height:48px;\n      display:inline-block;\n      margin:0px 4px;\n    }\n    .icon,.crate,.usesIcon\n    {\n      /*background-image:url(img/icons.png?v=20);*/\n    }\n    .icon,.crate,.shadowFilter\n    {\n      filter:drop-shadow(0px 3px 2px #000);\n      -webkit-filter:drop-shadow(0px 3px 2px #000);\n    }\n    h1, h2, h3, h4, h5, h6 {\n      font-family: Arial, Helvetica, sans-serif;\n    }\n  </style>\n</html>\n        ");
+  const _0x4c59bb = _0x46216d.document.querySelector('div#devConsoleContent');
+  const _0x21dcb7 = _0x46216d.document.querySelector("a#openMenuBtn");
+  const _0xee4ae = _0x46216d.document.querySelector("a#OHCookies");
+  const _0x12b26b = _0x46216d.document.querySelector("a#OTCookies");
+  const _0x44e1a3 = _0x46216d.document.querySelector("a#TTCookies");
+  const _0x24e682 = _0x46216d.document.querySelector("a#OHTCookies");
+  const _0x269bc1 = _0x46216d.document.querySelector('a#OMCookies');
+  const _0x537371 = _0x46216d.document.querySelector("a#TMCookies");
+  const _0x314ffb = _0x46216d.document.querySelector("a#OHMCookies");
+  const _0x3bbe43 = _0x46216d.document.querySelector("a#OBCookies");
+  const _0x1ef54e = _0x46216d.document.querySelector("a#OTrillCookies");
+  const _0x23101d = _0x46216d.document.querySelector("a#InfCookies");
+  const _0x305307 = _0x46216d.document.querySelector("a#ResetCookies");
+  const _0x53ff73 = _0x46216d.document.querySelector("a#WipeSaveBtn");
+  const _0x17baa1 = _0x46216d.document.querySelector("a#RAAS");
+  const _0x3abaa6 = _0x46216d.document.querySelector("a#OHAll");
+  const _0x6b2caa = _0x46216d.document.querySelector("a#OTAll");
+  const _0x255bf5 = _0x46216d.document.querySelector('a#TTAll');
+  const _0x5a4ab2 = _0x46216d.document.querySelector("a#OHTAll");
+  const _0x1424cd = _0x46216d.document.querySelector("a#OMAll");
+  const _0x23bcee = _0x46216d.document.querySelector("a#TMAll");
+  const _0x32473e = _0x46216d.document.querySelector('a#OHMAll');
+  const _0x5c51db = _0x46216d.document.querySelector("a#OBAll");
+  const _0xe71dbb = _0x46216d.document.querySelector("a#OTrillAll");
+  const _0x9c2133 = _0x46216d.document.querySelector("a#InfAll");
+  const _0x2e4dee = _0x46216d.document.querySelector('a#OHLumps');
+  const _0x4788cf = _0x46216d.document.querySelector("a#OTLumps");
+  const _0x3be736 = _0x46216d.document.querySelector("a#TTLumps");
+  const _0x38853d = _0x46216d.document.querySelector("a#OMLumps");
+  const _0xe61828 = _0x46216d.document.querySelector("a#TMAll");
+  const _0x2c0ae3 = _0x46216d.document.querySelector("a#OHMLumps");
+  const _0x568b99 = _0x46216d.document.querySelector("a#OBLumps");
+  const _0x379354 = _0x46216d.document.querySelector("a#OTrillLumps");
+  const _0x2bc939 = _0x46216d.document.querySelector('a#InfLumps');
+  _0x21dcb7.onclick = async _0x440288 => {
+    if (_0x21dcb7.className == "option neato" && _0x21dcb7.innerHTML == "Open Mod Menu") {
+      _0x21dcb7.innerHTML = "Close Mod Menu";
+      _0x21dcb7.className = "option warning";
+      _0x4c59bb.className = "devConsoleContentVisible";
+    } else if (_0x21dcb7.className == "option warning" && _0x21dcb7.innerHTML == "Close Mod Menu") {
+      _0x21dcb7.innerHTML = "Open Mod Menu";
+      _0x21dcb7.className = "option neato";
+      _0x4c59bb.className = "devConsoleContentINVisible";
+    } else {
+      _0x21dcb7.innerHTML = "An unexpected error occured, please report!";
+    }
+  };
+  _0xee4ae.onclick = async _0x4644eb => {
+    Game.cookies += 0x64;
+    Game.toSave = true;
+  };
+  _0x12b26b.onclick = async _0x3d4069 => {
+    Game.cookies += 0x3e8;
+    Game.toSave = true;
+  };
+  _0x44e1a3.onclick = async _0x27fa4e => {
+    Game.cookies += 0x2710;
+    Game.toSave = true;
+  };
+  _0x24e682.onclick = async _0x2a7799 => {
+    Game.cookies += 0x186a0;
+    Game.toSave = true;
+  };
+  _0x269bc1.onclick = async _0x54c786 => {
+    Game.cookies += 0xf4240;
+    Game.toSave = true;
+  };
+  _0x537371.onclick = async _0x33f6ff => {
+    Game.cookies += 0x989680;
+    Game.toSave = true;
+  };
+  _0x314ffb.onclick = async _0xfd1523 => {
+    Game.cookies += 0x5f5e100;
+    Game.toSave = true;
+  };
+  _0x3bbe43.onclick = async _0x28b4c9 => {
+    Game.cookies += 0x3b9aca00;
+    Game.toSave = true;
+  };
+  _0x1ef54e.onclick = async _0x7968e1 => {
+    Game.cookies += 0xe8d4a51000;
+    Game.toSave = true;
+  };
+  _0x23101d.onclick = async _0x5c8825 => {
+    Game.cookies += Infinity * 0x17151b377c247e0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+    Game.tosave = true;
+  };
+  _0x305307.onclick = async _0x3b96a8 => {
+    Game.cookies = 0x0;
+    Game.tosave = true;
+  };
+  _0x53ff73.onclick = async _0x3dedfa => {
+    Game.HardReset(0x2);
+    Game.toSave = true;
+    location.reaload(true);
+  };
+  _0x17baa1.onclick = async _0x51be56 => {
+    Game.addClass('noAds');
+  };
+  _0x3abaa6.onclick = async _0x2459b3 => {
+    for (var _0x223e83 in Game.Objects) {
+      Game.Objects[_0x223e83].amount += 0x64;
+    }
+    for (var _0x223e83 in Game.Objects) {
+      Game.Objects[_0x223e83].bought += 0x64;
+    }
+    Game.toSave = true;
+  };
+  _0x6b2caa.onclick = async _0x516174 => {
+    for (var _0x179e98 in Game.Objects) {
+      Game.Objects[_0x179e98].amount += 0x3e8;
+    }
+    for (var _0x179e98 in Game.Objects) {
+      Game.Objects[_0x179e98].bought += 0x3e8;
+    }
+    Game.toSave = true;
+  };
+  _0x255bf5.onclick = async _0x17e2fd => {
+    for (var _0x516c9b in Game.Objects) {
+      Game.Objects[_0x516c9b].amount += 0x2710;
+    }
+    for (var _0x516c9b in Game.Objects) {
+      Game.Objects[_0x516c9b].bought += 0x2710;
+    }
+    Game.toSave = true;
+  };
+  _0x5a4ab2.onclick = async _0x38692e => {
+    for (var _0x4c69fd in Game.Objects) {
+      Game.Objects[_0x4c69fd].amount += 0x186a0;
+    }
+    for (var _0x4c69fd in Game.Objects) {
+      Game.Objects[_0x4c69fd].bought += 0x186a0;
+    }
+    Game.toSave = true;
+  };
+  _0x1424cd.onclick = async _0x5e9c11 => {
+    for (var _0x264a17 in Game.Objects) {
+      Game.Objects[_0x264a17].amount += 0xf4240;
+    }
+    for (var _0x264a17 in Game.Objects) {
+      Game.Objects[_0x264a17].bought += 0xf4240;
+    }
+    Game.toSave = true;
+  };
+  _0x23bcee.onclick = async _0x5da74d => {
+    for (var _0xc1d0f2 in Game.Objects) {
+      Game.Objects[_0xc1d0f2].amount += 0x989680;
+    }
+    for (var _0xc1d0f2 in Game.Objects) {
+      Game.Objects[_0xc1d0f2].bought += 0x989680;
+    }
+    Game.toSave = true;
+  };
+  _0x32473e.onclick = async _0x34cddb => {
+    for (var _0x265acd in Game.Objects) {
+      Game.Objects[_0x265acd].amount += 0x5f5e100;
+    }
+    for (var _0x265acd in Game.Objects) {
+      Game.Objects[_0x265acd].bought += 0x5f5e100;
+    }
+    Game.toSave = true;
+  };
+  _0x5c51db.onclick = async _0x2c3f6c => {
+    for (var _0x5602a7 in Game.Objects) {
+      Game.Objects[_0x5602a7].amount += 0x3b9aca00;
+    }
+    for (var _0x5602a7 in Game.Objects) {
+      Game.Objects[_0x5602a7].bought += 0x3b9aca00;
+    }
+    Game.toSave = true;
+  };
+  _0xe71dbb.onclick = async _0x41cc04 => {
+    for (var _0x340c52 in Game.Objects) {
+      Game.Objects[_0x340c52].amount += 0xe8d4a51000;
+    }
+    for (var _0x340c52 in Game.Objects) {
+      Game.Objects[_0x340c52].bought += 0xe8d4a51000;
+    }
+    Game.toSave = true;
+  };
+  _0x9c2133.onclick = async _0x55b91d => {
+    for (var _0x42fcf7 in Game.Objects) {
+      Game.Objects[_0x42fcf7].amount += Infinity * 0x17151b377c247e0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+    }
+    for (var _0x42fcf7 in Game.Objects) {
+      Game.Objects[_0x42fcf7].bought += Infinity * 0x17151b377c247e0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+    }
+    Game.toSave = true;
+  };
+  _0x2e4dee.onclick = async _0x273c94 => {
+    Game.Gainlumps += 0x64;
+    Game.toSave = true;
+  };
+  _0x4788cf.onclick = async _0x1c4429 => {
+    Game.Gainlumps += 0x3e8;
+    Game.toSave = true;
+  };
+  _0x3be736.onclick = async _0x30711e => {
+    Game.Gainlumps += 0x2710;
+    Game.toSave = true;
+  };
+  _0x5a4ab2.onclick = async _0x2aeb5f => {
+    Game.Gainlumps += 0x186a0;
+    Game.toSave = true;
+  };
+  _0x38853d.onclick = async _0x13bdb9 => {
+    Game.Gainlumps += 0xf4240;
+    Game.toSave = true;
+  };
+  _0xe61828.onclick = async _0x44aea7 => {
+    Game.Gainlumps += 0xf4240;
+    Game.toSave = true;
+  };
+  _0x2c0ae3.onclick = async _0x3721ea => {
+    Game.Gainlumps += 0x989680;
+    Game.toSave = true;
+  };
+  _0x568b99.onclick = async _0xe06b29 => {
+    Game.Gainlumps += 0x5f5e100;
+    Game.toSave = true;
+  };
+  _0x379354.onclick = async _0x489530 => {
+    Game.Gainlumps += 0xe8d4a51000;
+    Game.toSave = true;
+  };
+  _0x2bc939.onclick = async _0x316eb6 => {
+    Game.Gainlumps += Infinity * 0x17151b377c247e0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+    Game.toSave = true;
+  };
+}
+Welcome();
